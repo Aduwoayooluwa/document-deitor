7cad1bfe24ec4fb1a2bb7ce4514ef7b9b3b9936f
Aduwo Ayooluwa <Aduwo.Ayooluwa@sterling.ng>
2022-10-21 15:52:36 +0100

    ts app

---
+++ src/football/components/Navbar/navItems.ts
@@ -0,0 +1,19 @@
+export const navItems = [
+    {
+        title: 'Home',
+        to: "/",
+        id: "home"
+    },
+    {
+        title: 'News',
+        to: "/news",
+        id: "news"
+    },
+    {
+        title: 'Signin',
+        to: "/signin",
+        id: "signin"
+    },
+
+
+]
