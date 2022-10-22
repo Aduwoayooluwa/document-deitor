c0c69f25cd1bfd5b5dbf6ebec0fa28260daf09ec
Aduwo Ayooluwa <Aduwo.Ayooluwa@sterling.ng>
2022-10-22 10:10:34 +0100

    cchainging project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from './football/components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
         <div className='Main'>
-          <Navbar />
+          
         </div>
     </div>
   );
